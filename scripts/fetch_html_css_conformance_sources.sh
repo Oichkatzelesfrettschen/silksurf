bf27935f022304a890f12d223152bfe144eff13b
eirikr <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2026-07-02 00:29:08 -0700

    perf: paint viewport directly in ARGB

--- scripts/fetch_html_css_conformance_sources.sh
+++ scripts/fetch_html_css_conformance_sources.sh
@@ -22,7 +22,9 @@ mkdir -p "$bundle_dir"
 
 fetch_source css21.pdf https://www.w3.org/TR/CSS21/css2.pdf
 fetch_source css22.pdf https://www.w3.org/TR/CSS22/css2.pdf
+fetch_source css2-1998.pdf https://www.w3.org/TR/1998/REC-CSS2-19980512/css2.pdf
 fetch_source html40.pdf https://www.w3.org/TR/1998/REC-html40-19980424/html40.pdf
+fetch_source html401.pdf.gz https://www.w3.org/TR/1999/REC-html401-19991224/html40.pdf.gz
 fetch_source html-living-standard.html https://html.spec.whatwg.org/multipage/
 fetch_source css-syntax-3.html https://www.w3.org/TR/css-syntax-3/
 fetch_source selectors-4.html https://www.w3.org/TR/selectors-4/
@@ -33,11 +35,27 @@ fetch_source css-backgrounds-3.html https://www.w3.org/TR/css-backgrounds-3/
 fetch_source css-flexbox-1.html https://www.w3.org/TR/css-flexbox-1/
 fetch_source css-display-3.html https://www.w3.org/TR/css-display-3/
 fetch_source css-text-3.html https://www.w3.org/TR/css-text-3/
+fetch_source cssom-1.html https://www.w3.org/TR/cssom-1/
+fetch_source cssom-view-1.html https://www.w3.org/TR/cssom-view-1/
+fetch_source css-sizing-3.html https://www.w3.org/TR/css-sizing-3/
+fetch_source css-overflow-3.html https://www.w3.org/TR/css-overflow-3/
+fetch_source css-position-3.html https://www.w3.org/TR/css-position-3/
+fetch_source css-align-3.html https://www.w3.org/TR/css-align-3/
+fetch_source css-fonts-4.html https://www.w3.org/TR/css-fonts-4/
+fetch_source css-images-3.html https://www.w3.org/TR/css-images-3/
+
+for html_file in "$tmp_dir"/*.html
+do
+    [ -e "$html_file" ] || continue
+    sed -i 's/\r$//' "$html_file"
+done
 
 for file in \
     css21.pdf \
     css22.pdf \
+    css2-1998.pdf \
     html40.pdf \
+    html401.pdf.gz \
     html-living-standard.html \
     css-syntax-3.html \
     selectors-4.html \
@@ -47,7 +65,15 @@ for file in \
     css-backgrounds-3.html \
     css-flexbox-1.html \
     css-display-3.html \
-    css-text-3.html
+    css-text-3.html \
+    cssom-1.html \
+    cssom-view-1.html \
+    css-sizing-3.html \
+    css-overflow-3.html \
+    css-position-3.html \
+    css-align-3.html \
+    css-fonts-4.html \
+    css-images-3.html
 do
     mv "$tmp_dir/$file" "$bundle_dir/$file"
 done
@@ -57,7 +83,9 @@ done
     sha256sum \
         css21.pdf \
         css22.pdf \
+        css2-1998.pdf \
         html40.pdf \
+        html401.pdf.gz \
         html-living-standard.html \
         css-syntax-3.html \
         selectors-4.html \
@@ -67,6 +95,14 @@ done
         css-backgrounds-3.html \
         css-flexbox-1.html \
         css-display-3.html \
-        css-text-3.html >SHA256SUMS
+        css-text-3.html \
+        cssom-1.html \
+        cssom-view-1.html \
+        css-sizing-3.html \
+        css-overflow-3.html \
+        css-position-3.html \
+        css-align-3.html \
+        css-fonts-4.html \
+        css-images-3.html >SHA256SUMS
     sha256sum -c SHA256SUMS
 )
